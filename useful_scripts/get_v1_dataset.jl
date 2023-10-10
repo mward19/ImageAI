3124ad6593e5206d22bc1c7564a64a7a6eac7a7d
jblaser2 <144287726+jblaser2@users.noreply.github.com>
2023-10-10 13:44:22 -0600

    Update get_v1_dataset.jl

--- useful_scripts/get_v1_dataset.jl
+++ useful_scripts/get_v1_dataset.jl
@@ -16,21 +16,61 @@ slicer_angles=$(imodinfo -a "$slicer_angles_file" | grep '^slicerAngle' | awk '{
  end
 
 
- function make_frames()
+ function make_frames(img, num_frames)
 #= Function to grab random frames from ratatevol output
 # input: img, num_frames
 # output: set of v1 images for given tomogram =#
+  motor_location_x = size(img)[1]//2;
+  motor_location_y = size(img)[2]//2;
 
+  rad_mot = 30
+  cut_size = 180
+  trans = cut_size//2 - rad_mot
+  x_move = rand(-trans:trans);
+  y_move = rand(-trans,trans);
+
+  x_initial = (size(img)[1]//2 - cut_size)//2;
+  y_initial = (size(img)[2]//2 - cut_size)//2;
+
+  frame_x = x_initial + x_move;
+  frame_y = y_initial + y_move;
+
+  img[1] = frame_x;
+  img[2] = frame_y;
+  
+  for i:num_frames
+    frame = run()
+    append!(img_list, frame)
+  end
+    return img_list
  end
 
+ make_frames(rotate_volume())
 
- function get_files()
+ function get_files(dir)
+  files = Vector{AbstractString}()
+  entries = readdir(dir)
+
+  for entry in entries
+    full_path = joinpath(dir, entry)
+
+    if isfile(full_path) && endswith(entry,[".mod", ".rec"])
+      push!(files,full_path)
+    end
+  end
+  if isempty(files)
+    println("No files located in this directory")
+  else 
+    println("Found the following files:")
+    for i in files
+      println(i)
+    end
+  end
+ end
 #= Function to search through directory for required files
 # input: directory
 # output: .rec file , .mod file =#
 
-
- end
 
 
 
