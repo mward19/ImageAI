33cc5d74fb2ac35e51b27a915536b18811350007
elons3 <eben.lonsdale@gmail.com>
2024-06-12 18:44:54 -0600

    Update

--- czi_metadata/data_exploration_scripts/search_subdirs.py
+++ czi_metadata/data_exploration_scripts/search_subdirs.py
@@ -1,6 +1,13 @@
 import os
 import sys
 
+'''
+This script is designed to be run inside any of the 4 directories on the supercomputer to check for duplicates with
+the CZI database. You have to go in a few levels for it to search correctly; the supercomputer is weird like that.
+
+Author: Eben Lonsdale, 10 April 2024
+'''
+
 def check_subdirectories(search_file, dir_to_search, output_file):
     """
     This function checks if any subdirectories match the directory names from a file.
@@ -8,7 +15,7 @@ def check_subdirectories(search_file, dir_to_search, output_file):
     """
     # Read the file and get the directory names
     with open(search_file, 'r') as file:
-        directory_names = file.read().splitlines()
+        directory_names = file.read().splitlines() # file you're checking for duplicates against
 
     # Open the output file to write the common subdirectories
     with open(output_file, 'a') as file:
