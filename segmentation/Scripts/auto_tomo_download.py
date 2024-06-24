49b4dfc6da55912408b7022ea3ac727a507ac14b
elons3 <152450688+elons3@users.noreply.github.com>
2024-06-24 16:17:25 -0600

    Update auto_tomo_download.py

--- segmentation/Scripts/auto_tomo_download.py
+++ segmentation/Scripts/auto_tomo_download.py
@@ -6,7 +6,6 @@ client = Client()
 # IDs for run to download
 datasetid = sys.argv[1]
 runid = sys.argv[2]
-
 # get correct run and download it
 run = Run.find(client, query_filters=[Run.id == runid, Run.dataset.id == datasetid])[0]
 tomoid = run.tomogram_voxel_spacings[0].tomograms[0].id
@@ -14,7 +13,7 @@ tomogram = Tomogram.get_by_id(client, tomoid)
 tomogram.download_mrcfile()
 
 # rename and move the file
-file_name = '/Users/mward19/Documents/Segmentation/tomogram_seg/raw_tomograms/run_'+str(run.id)+'.mrc'
-os.rename(tomogram.name+'.mrc',file_name)
+file_path = '/Users/mward19/Documents/Segmentation/tomogram_seg/raw_tomograms/dataset_'+datasetid+'/run_'+runid+'.mrc'
+os.rename(tomogram.name+'.mrc',file_path)
 # so you can get the file name in bash as a variable
-print(file_name)
+print(file_path)
