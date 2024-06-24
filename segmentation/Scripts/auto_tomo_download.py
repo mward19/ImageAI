036806c7ad95de33c82d89afed1148e2cd5d21a9
elons3 <152450688+elons3@users.noreply.github.com>
2024-06-24 16:09:22 -0600

    Create auto_tomo_download.py
    file for segmentation pipeline to automate file downloads

---
+++ segmentation/Scripts/auto_tomo_download.py
@@ -0,0 +1,20 @@
+from cryoet_data_portal import Client, Run, Tomogram
+import sys
+import os
+
+client = Client()
+# IDs for run to download
+datasetid = sys.argv[1]
+runid = sys.argv[2]
+
+# get correct run and download it
+run = Run.find(client, query_filters=[Run.id == runid, Run.dataset.id == datasetid])[0]
+tomoid = run.tomogram_voxel_spacings[0].tomograms[0].id
+tomogram = Tomogram.get_by_id(client, tomoid)
+tomogram.download_mrcfile()
+
+# rename and move the file
+file_name = '/Users/mward19/Documents/Segmentation/tomogram_seg/raw_tomograms/run_'+str(run.id)+'.mrc'
+os.rename(tomogram.name+'.mrc',file_name)
+# so you can get the file name in bash as a variable
+print(file_name)
