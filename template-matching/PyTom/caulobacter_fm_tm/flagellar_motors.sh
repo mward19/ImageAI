900bdb776b2b77caab2999c25ee9e100a0102a35
elons3 <eben.lonsdale@gmail.com>
2024-07-10 15:53:47 -0600

    debugging

--- template-matching/PyTom/caulobacter_fm_tm/flagellar_motors.sh
+++ template-matching/PyTom/caulobacter_fm_tm/flagellar_motors.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-#SBATCH --time=01:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
-#SBATCH --nodes=1   # number of nodes
+#SBATCH --time=1:00:00 # walltime
+
+#SBATCH --ntasks=1 # number of processor cores (i.e. tasks)
+
+#SBATCH --nodes=1 # number of nodes
+
 #SBATCH --gpus=2
-#SBATCH -C 'kepler'   # features syntax (use quotes): -C 'a&b&c&d'
-#SBATCH --mem 20G
-#SBATCH -J "fm_tm_01"   # job name
+
+#SBATCH --export=NONE
+
+#SBATCH --mem 12G
 
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
