ef3b4f543ac868ea3bf91c5957f5e236656f97c9
elons3 <eben.lonsdale@gmail.com>
2024-07-10 15:30:19 -0600

    updated script using kepler nodes, hopefully better memory allocation. Also changed the low pass filter

--- template-matching/PyTom/caulobacter_fm_tm/flagellar_motors.sh
+++ template-matching/PyTom/caulobacter_fm_tm/flagellar_motors.sh
@@ -1,21 +1,13 @@
-#!/bin/bash --login
+#!/bin/bash
 
+#SBATCH --time=01:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gpus=2
+#SBATCH -C 'kepler'   # features syntax (use quotes): -C 'a&b&c&d'
+#SBATCH --mem=20G   # memory per CPU core
+#SBATCH -J "fm_tm_01"   # job name
 
-
-
-#SBATCH --time=00:05:00 # walltime
-
-#SBATCH --ntasks=1 # number of processor cores (i.e. tasks)
-
-#SBATCH --nodes=1 # number of nodes
-
-#SBATCH --gpus=4
-
-#SBATCH --export=NONE
-
-#SBATCH --mem 2G
-
-  
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 
@@ -25,8 +17,8 @@ export LD_LIBRARY_PATH=/apps/spack/root/opt/spack/linux-rhel9-haswell/gcc-13.2.0
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE 
 
-module load miniconda3/24.3.0-poykqmt
-module load cuda/12.4.1-pw6cogp
+module load miniconda3
+module load cuda
 
 source activate pytom
 
@@ -36,8 +28,7 @@ pytom_create_template.py \
  --output-voxel 14.08 \
  --box-size 101 \
  --center \
- --log info
-
+ --low-pass 35
 
 pytom_create_mask.py \
  -b 100 \
@@ -46,13 +37,13 @@ pytom_create_mask.py \
  --radius 45 \
  --sigma 1
   
-# pytom_match_template.py \
-#  -t /home/ejl62/template_matching_shared/pytom/flagellar_motor_tm/templates_masks/caulobacter_fm_template.mrc \
-#  -m /home/ejl62/template_matching_shared/pytom/flagellar_motor_tm/templates_masks/caulobacter_fm_mask.mrc \
-#  -v /home/ejl62/fsl_groups/grp_tomo_db1_d1/nobackup/archive/TomoDB1_d1/FlagellarMotor_P1/Caulobacter\ crescentus/flag_3_full.rec \
-#  -d /home/ejl62/template_matching_shared/pytom/flagellar_motor_tm/results \
-#  --voxel-size-angstrom 14.08 \
-#  --particle-diameter 80 \
-#  --tilt-angles -55 55 \
-#  --random-phase \
-#  -g 0 1 2 3
+pytom_match_template.py \
+ -t /home/ejl62/template_matching_shared/pytom/flagellar_motor_tm/templates_masks/caulobacter_fm_template.mrc \
+ -m /home/ejl62/template_matching_shared/pytom/flagellar_motor_tm/templates_masks/caulobacter_fm_mask.mrc \
+ -v /home/ejl62/fsl_groups/grp_tomo_db1_d1/nobackup/archive/TomoDB1_d1/FlagellarMotor_P1/Caulobacter\ crescentus/flag_3_full.rec \
+ -d /home/ejl62/template_matching_shared/pytom/flagellar_motor_tm/results \
+ --voxel-size-angstrom 14.08 \
+ --particle-diameter 85 \
+ --tilt-angles -60 60 \
+ --random-phase \
+ -g 0 1
